0babbf324e8a54c46939663d30522006fcd4257b
Steven6798 <stevenramirez6798@gmail.com>
2026-03-07 11:59:04 -0500

    Initial implementation of SRRArchTargetLowering, LowerFormalArguments, and LowerReturn functions. Implemented CanLowerReturn function

--- llvm/lib/Target/SRRArch/SRRArchISelLowering.cpp
+++ llvm/lib/Target/SRRArch/SRRArchISelLowering.cpp
@@ -25,7 +25,21 @@ using namespace llvm;
 SRRArchTargetLowering::SRRArchTargetLowering(const TargetMachine &TM,
                                              const SRRArchSubtarget &STI)
     : TargetLowering(TM, STI) {
-  llvm_unreachable("SRRArchTargetLowering not implemented yet");
+  // Set up the register classes.
+  addRegisterClass(MVT::i64, &SRRArch::GPRRegClass);
+
+  // Compute derived properties from the register classes
+  TRI = STI.getRegisterInfo();
+  computeRegisterProperties(TRI);
+
+  setStackPointerRegisterToSaveRestore(SRRArch::R1);
+
+  // Function alignments
+  setMinFunctionAlignment(Align(8));
+  setPrefFunctionAlignment(Align(8));
+
+  // Booleans always contain 0 or 1.
+  setBooleanContents(ZeroOrOneBooleanContent);
 }
 
 SDValue SRRArchTargetLowering::LowerOperation(SDValue Op,
@@ -47,7 +61,17 @@ SDValue SRRArchTargetLowering::LowerFormalArguments(
     SDValue Chain, CallingConv::ID CallConv, bool IsVarArg,
     const SmallVectorImpl<ISD::InputArg> &Ins, const SDLoc &DL,
     SelectionDAG &DAG, SmallVectorImpl<SDValue> &InVals) const {
-  llvm_unreachable("LowerFormalArguments not implemented yet");
+  // Assign locations to all of the incoming arguments.
+  SmallVector<CCValAssign, 16> ArgLocs;
+  CCState CCInfo(CallConv, IsVarArg, DAG.getMachineFunction(), ArgLocs,
+                 *DAG.getContext());
+  CCInfo.AnalyzeFormalArguments(Ins, CC_SRRArch);
+
+  for (const CCValAssign &VA : ArgLocs) {
+    llvm_unreachable("Arguments not supported yet");
+  }
+
+  return Chain;
 }
 
 SDValue
@@ -60,7 +84,10 @@ bool SRRArchTargetLowering::CanLowerReturn(
     CallingConv::ID CallConv, MachineFunction &MF, bool IsVarArg,
     const SmallVectorImpl<ISD::OutputArg> &Outs, LLVMContext &Context,
     const Type *RetTy) const {
-  llvm_unreachable("CanLowerReturn not implemented yet");
+  SmallVector<CCValAssign, 16> RVLocs;
+  CCState CCInfo(CallConv, IsVarArg, MF, RVLocs, Context);
+
+  return CCInfo.CheckReturn(Outs, RetCC_SRRArch);
   return false;
 }
 
@@ -70,7 +97,26 @@ SRRArchTargetLowering::LowerReturn(SDValue Chain, CallingConv::ID CallConv,
                                    const SmallVectorImpl<ISD::OutputArg> &Outs,
                                    const SmallVectorImpl<SDValue> &OutVals,
                                    const SDLoc &DL, SelectionDAG &DAG) const {
-  llvm_unreachable("LowerReturn not implemented yet");
+  // CCValAssign - represent the assignment of the return value to a location
+  SmallVector<CCValAssign, 16> RVLocs;
+
+  // CCState - Info about the registers and stack slot.
+  CCState CCInfo(CallConv, IsVarArg, DAG.getMachineFunction(), RVLocs,
+                 *DAG.getContext());
+
+  // Analize return values.
+  CCInfo.AnalyzeReturn(Outs, RetCC_SRRArch);
+
+  SmallVector<SDValue, 4> RetOps(1, Chain);
+
+  // Copy the result values into the output registers.
+  for (unsigned i = 0; i != RVLocs.size(); ++i) {
+    llvm_unreachable("Return values not supported yet");
+  }
+
+  unsigned Opc = SRRArchISD::RET_GLUE;
+  return DAG.getNode(Opc, DL, MVT::Other,
+                     ArrayRef<SDValue>(&RetOps[0], RetOps.size()));
 }
 
 //===----------------------------------------------------------------------===//
